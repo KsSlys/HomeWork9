f06cddbea9120445b832b89beb5569bd4636bb67
Ksenia Slyshinskaya <ksenia_slys@mail.ru>
2023-03-13 11:37:31 +0300

    Ex

---
+++ Ex.66/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {SumNumbers(m,n)}");
+
+int SumNumbers (int a, int b)
+{
+    if (a==b) return a;
+    else return a + SumNumbers(a + 1 , b);
+
+}
+
---
+++ Ex.68/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+Console.Clear();
+Console.WriteLine("Введите первое положительное число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе положительное число");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(FunctionAck(m , n));
+
+int FunctionAck(int a, int b)
+{
+    if (a == 0) return b +1;
+    else if (b == 0) return FunctionAck(a - 1, 1);
+    else return FunctionAck(a - 1, FunctionAck(a, b-1));
+}
